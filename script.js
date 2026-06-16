e3c61ecf2412fcc89d1a437e3809ca5542c36797
Andrej Bajusic <andrejbajusic2012@gmail.com>
2026-06-16 08:38:40 +0200

    Enhance countdown display with hours and seconds

--- script.js
+++ script.js
@@ -10,9 +10,16 @@ const ferien=document.getElementById("ferien");
         var kinderfestdiff= kinderfestdate-now
         var feriendiff= feriendate-now
         const kinderfestdays = Math.ceil(kinderfestdiff / (1000 * 60 * 60 * 24));
-        const feriendays = Math.ceil(feriendiff / (1000 * 60 * 60 * 24));
-        ferien.textContent = feriendays + " Tage";
-        kinderfest.textContent = kinderfestdays + " Tage"; 
+        const feriendays = Math.ceil(feriendiff / (1000 * 60 * 60 * 24));       
+        
+        const kinderfesth = Math.ceil(kinderfestdiff / (1000 * 60 * 60 ));
+        const ferienh = Math.ceil(feriendiff / (1000 * 60 * 60));
+
+        const kinderfests = Math.ceil(kinderfestdiff / (1000 * 60 ));
+        const feriens = Math.ceil(feriendiff / (1000 * 60));
+        
+        ferien.textContent = feriendays + " Tage, " + ferienh + " Stunden, "+ feriens + " Sekunden";
+        kinderfest.textContent = kinderfestdays + " Tage, " + kinderfesth + " Stunden, "+ kinderfests + " Sekunden"; 
     }
     countdownupdate()
     setInterval(countdownupdate, 60000);
